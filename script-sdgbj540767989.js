f90ff128ab252f88f9efb8bf8f7146a5ca2c344a
SAW-COIN <abasfacre@gmail.com>
2024-12-22 03:42:08 +0200

    Update script-sdgbj540767989.js

--- script-sdgbj540767989.js
+++ script-sdgbj540767989.js
@@ -1,9 +1,8 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
 
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
-);
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // تعريف عناصر DOM
 const uiElements = {
