cfe239af8dc60c1b988e08d362c2b80683183b55
SAW-COIN <abasfacre@gmail.com>
2024-12-22 03:44:51 +0200

    Update script-sdgbj540767989.js

--- script-sdgbj540767989.js
+++ script-sdgbj540767989.js
@@ -1,6 +1,5 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from './Scripts/config.js';
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
