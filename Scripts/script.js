d57ec4e7a4d78c2082bcd6983002268919864a3c
elsoghiar <mahmoudhouseen555@gmail.com>
2024-12-30 00:11:42 +0200

    Update script.js

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,1 +1,68 @@
-function b(c,d){var e=a();return b=function(f,g){f=f-(-0xc9*-0x1d+0x3db*0x2+-0x1ceb*0x1);var h=e[f];return h;},b(c,d);}function a(){var l=['preventDefault','61796ICENwz','17eXvauh','1507137FdVTZj','forEach','img,\x20p','148uEJEXi','mousedown','1597086IXCrnq','137778ybpOUt','20jlBqzR','12935gMNPrV','contextmenu','8962426FNzfvF','addEventListener','2154248smKfZo','7SLWRrO'];a=function(){return l;};return a();}var i=b;(function(c,d){var h=b,e=c();while(!![]){try{var f=parseInt(h(0x19b))/(-0x13e5+0x17af*-0x1+0x3*0xe87)*(parseInt(h(0x19a))/(-0xf15+-0x20a9+0x40*0xbf))+-parseInt(h(0x19c))/(0x7c5+0x1*-0x1066+0x8a4)+parseInt(h(0x19f))/(-0x1a*-0x5d+-0x1*-0x3f5+-0xd63)*(parseInt(h(0x193))/(-0x40*0x5f+-0x293*0xb+0x3416))+-parseInt(h(0x191))/(0x218d+0x3*-0x72a+-0xc09)+-parseInt(h(0x198))/(0x8de+0xa57*0x1+-0xa*0x1eb)*(parseInt(h(0x197))/(-0x22c6+-0x177b+0x2b*0x15b))+parseInt(h(0x190))/(-0x1*0xae5+-0x1473+-0x1f61*-0x1)*(-parseInt(h(0x192))/(-0x1*-0x502+0x361+-0x859))+parseInt(h(0x195))/(-0x18a*0x12+-0x78d+-0x9*-0x3ec);if(f===d)break;else e['push'](e['shift']());}catch(g){e['push'](e['shift']());}}}(a,-0x2b*-0x727+0x5cbc4+-0x2a145),document[i(0x196)](i(0x194),function(c){var j=i;c[j(0x199)]();}),document['querySelectorAll'](i(0x19e))[i(0x19d)](c=>{var k=i;c[k(0x196)](k(0x1a0),d=>d['preventDefault']()),c['addEventListener']('touchstart',d=>d[k(0x199)]());}));
+// 1. تعطيل القائمة السياقية (Right Click)
+document.addEventListener("contextmenu", function(event) {
+    event.preventDefault();
+});
+
+// 2. تعطيل الضغط المطوّل على العناصر
+document.querySelectorAll("img, p, div, span").forEach(element => {
+    element.addEventListener("mousedown", e => e.preventDefault());
+    element.addEventListener("touchstart", e => e.preventDefault());
+});
+
+// 3. منع اختصارات لوحة المفاتيح الشائعة
+document.addEventListener("keydown", function(event) {
+    // اختصارات شائعة مثل Ctrl+U, Ctrl+Shift+I, Ctrl+S, F12
+    if (event.ctrlKey && (event.key === "u" || event.key === "U" || 
+                          event.key === "i" || event.key === "I" || 
+                          event.key === "s" || event.key === "S")) {
+        event.preventDefault();
+    }
+    if (event.key === "F12") {
+        event.preventDefault();
+    }
+});
+
+// 4. تعطيل السحب والإفلات على العناصر
+document.querySelectorAll("img, p, div, span").forEach(element => {
+    element.setAttribute("draggable", "false");
+    element.addEventListener("dragstart", e => e.preventDefault());
+});
+
+// 5. منع تحديد النصوص
+document.addEventListener("selectstart", function(event) {
+    event.preventDefault();
+});
+
+// 6. منع النسخ واللصق
+document.addEventListener("copy", function(event) {
+    event.preventDefault();
+    alert("forbidden.");
+});
+
+
+// 7. اكتشاف أدوات المطور
+(function() {
+    const detectDevTools = () => {
+        const widthThreshold = window.outerWidth - window.innerWidth > 200;
+        const heightThreshold = window.outerHeight - window.innerHeight > 200;
+        if (widthThreshold || heightThreshold) {
+            alert("forbidden");
+            window.close();
+        }
+    };
+    setInterval(detectDevTools, 1000);
+})();
+
+// 8. منع التحميل داخل Iframe
+if (window.top !== window.self) {
+    window.top.location = window.self.location;
+}
+
+// 10. منع استخدام Inspect Element
+document.addEventListener("keydown", function(event) {
+    if (event.ctrlKey && event.shiftKey && event.key === "I") {
+        event.preventDefault();
+        alert("forbidden");
+    }
+});
+
